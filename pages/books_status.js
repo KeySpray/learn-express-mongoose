9193729be285a80d833e117f273e54b7a0a145e2
Daniel_Chinn <danielrchinn@gmail.com>
2024-03-19 22:50:32 -0400

    class activities

--- pages/books_status.js
+++ pages/books_status.js
@@ -1,5 +1,22 @@
 let BookInstance = require('../models/bookinstance');
 
-exports.show_all_books_status = function(res) {
-  return res.send([]);
+getBooks = async () => {
+  return BookInstance.find({'status': {$eq: 'Available'}})
+    .populate('book')
+    .exec()
+    .then(list_bookinstances => {
+      return list_bookinstances.map(instance => ({
+        title: instance.book.title, 
+        status: instance.status
+      }));
+    })
+    .catch(err => {
+      console.error('Error fetching available book instances:', err);
+      throw err; 
+    });
+}
+
+exports.show_all_books_status = function(req, res) {
+  var books = getBooks();
+  return res.send(books);
 }
